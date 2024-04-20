5373ee2666650ed9c16d217cb77b27957ee04da7
matt-colen <mzcolen@gmail.com>
2024-04-20 11:23:40 -0500

    Updates background color

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import projects from "../scripts/projects.js";
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
+
+// document.body.addEventListener("click", (e) => {
+//   if (e.target.id === "ham-btn" || e.target.id === "nav-drawer-close-btn") {
+//     document.querySelector("#nav-drawer").classList.toggle("hidden");
+//   }
+// });
+
+// const getCardContents = (arr) => {
+//   let html = "";
+
+//   arr.forEach((project, i) => {
+//     const side = i % 2 === 0 ? "right" : "left";
+//     const oppositeSide = side === "right" ? "left" : "right";
+//     const flipped = i % 2 === 0 ? "card" : "card card--flipped";
+//     const tagsHTML = project.tags.map((tag) => {
+//       return `<li class="tag">${tag}</li>`;
+//     });
+
+//     html += `
+//     <div class="${flipped} card--projects">
+//       <div class="card-${oppositeSide} flex">
+//         <div class="card-inner">
+//           <h3>${project.name}</h3>
+//           <p>${project.description}</p>
+//           <div class="tags">
+//             <ul class="flex tags-ul">
+//               ${tagsHTML.join("")}
+//             </ul>
+//           </div>
+//           <div class="project-links__container flex">
+//             <a
+//               href="${project.link}"
+//               class="btn btn--primary"
+//               target="_blank"
+//               >View Project</a
+//             >
+//             <a
+//               href="${project.github}"
+//               class="btn btn--alt"
+//               target="_blank"
+//               >View GitHub</a
+//             >
+//           </div>
+//         </div>
+//       </div>
+//       <div class="card-${side}">
+//         <img
+//           class="project-img"
+//           src="${project.thumbnail}"
+//           alt=""
+//         />
+//       </div>
+//     </div>
+//     `;
+//   });
+
+//   renderProjects(html);
+// };
+
+// const renderProjects = (html) => {
+//   document.querySelector("#projects__list").innerHTML = html;
+// };
+
+// getCardContents(projects);
+
+// const renderCopyright = () => {
+//   document.querySelector(
+//     "#copyright"
+//   ).innerHTML = `<p>&copy ${new Date().getFullYear()} Matt Colen. All Rights Reserved `;
+// };
+
+// renderCopyright();
---
+++ src/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    name: "Twimba",
+    description: "A clone of the Twitter (X) app",
+    tags: ["HTML", "CSS", "JavaScript", "Firebase"],
+    thumbnail: "../assets/images/twimba.png",
+    link: "https://twimba-mc.netlify.app/",
+    github: "https://github.com/matt-colen/twimba",
+  },
+  {
+    name: "Groovy Pups Spa",
+    description: "A website for a local dog bathing business",
+    tags: ["HTML", "CSS", "JavaScript"],
+    thumbnail: "../assets/images/groovy-pups-spa.png",
+    link: "https://groovy-pups-spa.netlify.app/",
+    github: "https://github.com/matt-colen/groovy-pups-spa",
+  },
+  {
+    name: "We Are The Champions",
+    description: "A website for teams to share messages",
+    tags: ["HTML", "CSS", "JavaScript", "Firebase"],
+    thumbnail: "../assets/images/we-are-the-champions.png",
+    link: "https://we-are-the-champions-mc.netlify.app/",
+    github: "https://github.com/matt-colen/we-are-the-champions",
+  },
+];
+
+export default projects;
