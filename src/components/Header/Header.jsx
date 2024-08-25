d428a099a46784e3035fc615103a3e11111bdc46
matt-colen <mzcolen@gmail.com>
2024-08-25 18:53:35 -0500

    Adds initial hero background animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,29 @@
 import Nav from "./Nav/Nav.jsx";
 import Hero from "./Hero/Hero.jsx";
+import { nanoid } from "nanoid";
 
 import "./Header.css";
+import { animation } from "terminal-kit/lib/spChars.js";
 
 export default function Header() {
+  const boxNumbers = [];
+
+  for (let i = 1; i < 73; i++) {
+    boxNumbers.push(i);
+  }
+
+  const boxElements = boxNumbers.map((num) => {
+    const style = {
+      animation: `box-shadows 10s ${Math.floor(Math.random() * num)}s infinite`,
+    };
+    return <div key={nanoid()} style={style} className="box"></div>;
+  });
+
   return (
-    <header>
+    <header className="header">
+      <div className="header-background">
+        {boxElements}
+      </div>
       <Nav />
       <Hero />
     </header>
--- src/components/Header/Hero/Hero.jsx
+++ src/components/Header/Hero/Hero.jsx
@@ -4,7 +4,9 @@ export default function Hero() {
   return (
     <section className="hero flex">
       <h1 className="title">Matt Colen</h1>
-      <p>Crafting seamless user experiences through code...</p>
+      <p className="sub-title">
+        Crafting seamless user experiences through code...
+      </p>
     </section>
   );
 }
